c61b62be51a1194ffe82a430d7e35152fe97916c
yesjuhee <juhee0924@g.skku.edu>
2026-03-18 15:48:18 +0900

    feat: 인자 2개를 받아 사칙연산을 할 수 있는 계산기를 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Calculator {
+
+    int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
+
